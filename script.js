8d2e7e7f56b9b61e5057b9c15bf0877db1d5b178
Vladimir Jancar <jvladko@gmail.com>
2025-06-03 02:59:27 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const dataUrl = "cv-data.json";
+  const dataUrl = "cv-data-sk.json";
 
   async function loadCVData() {
     try {
@@ -12,7 +12,8 @@ document.addEventListener("DOMContentLoaded", () => {
       initializePopups();
     } catch (error) {
       console.error("Could not load CV data:", error);
-      document.body.innerHTML = "<h1>Error loading CV data.</h1>";
+      document.body.innerHTML =
+        "<p style='color:#ff0000;font-weight:bold;font-size:2rem;text-align:center;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);padding:1rem 2rem;background-color:rgba(255,0,0,0.1);border:2px solid #ff0000;border-radius:5px;box-shadow:0 0 10px rgba(255,0,0,0.3);z-index:1000;'>Error loading CV data.</p>";
     }
   }
 
